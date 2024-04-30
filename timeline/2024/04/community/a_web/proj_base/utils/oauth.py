2ada2a473c1f6ff67c4bbbc622bcb665b050855b
Nahida-aa <1276552337@qq.com>
2024-04-30 20:56:33 +0800

    oauth_github_2_userinfo

--- timeline/2024/04/community/a_web/proj_base/utils/oauth.py
+++ timeline/2024/04/community/a_web/proj_base/utils/oauth.py
@@ -4,8 +4,18 @@ def github_login_url(client_id, redirect_url):
     github登录
     :param client_id: appID
     :param redirect_url:  your github redirect_url
-    :return: 用户名
+    :return: github登录url
     """
     g_l_url = f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_url}"
     return g_l_url
+
+def github_info(client_id, client_secret, code):
+    """
+    获取github用户信息
+    :param client_id: appID
+    :param client_secret: appSecret
+    :param code: github返回的code
+    :return: 用户信息
+    """
+    pass
     
