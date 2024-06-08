6f7287bca58df96787f91f92b89d6a2f487e6db0
aa <Nahida-aa@outlook.com>
2024-06-09 07:39:10 +0800

    xx

--- timeline/2024/04/community/a_web/proj_base/apps/oauth/urls.py
+++ timeline/2024/04/community/a_web/proj_base/apps/oauth/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('qq/login_url/', QQLoginURL.as_view(), name='qq_login_url'),
-    path('qq/user_info/', QQUserInfo.as_view(), name='qq_user_info'),
+    # path('qq/login_url/', QQLoginURL.as_view(), name='qq_login_url'),
+    # path('qq/user_info/', QQUserInfo.as_view(), name='qq_user_info'),
     path('github/login_url/', GithubLoginURL.as_view(), name='github_login_url'),
     path('github/login_auth/', GithubLoginAuth.as_view(), name='github_user_auth'),
 ]
