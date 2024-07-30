2e632d945796feb9df9c47b53f92b37f717774f4
aa <Nahida-aa@outlook.com>
2024-07-31 01:12:11 +0800

    h5_init

---
+++ timeline/2023/10/github_pages_nextjs/src/components/UserInfo/UserInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    <image
+      src="/static/imgs/Nahida.jpg"
+      mode="scaleToFill"
+      class="avatar"
+    />
+    <view class="username">Nahida</view>
+  </view>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+    
+      
