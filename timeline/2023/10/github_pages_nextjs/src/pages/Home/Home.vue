4ec1edc623375d6fd7d0fdce42ee31b0b4301be1
aa <Nahida-aa@outlook.com>
2024-07-31 15:59:59 +0800

    home update

--- timeline/2023/10/github_pages_nextjs/src/pages/Home/Home.vue
+++ timeline/2023/10/github_pages_nextjs/src/pages/Home/Home.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Main from '../../components/main/main.vue'
+import SkyJs from '../../components/bg/sky_js.vue';
 
 const title = ref('Home')
 
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome to the home page!</p>
-  <Main></Main>
+  <SkyJs>
+    <h1>{{ title }}</h1>
+    <p>Welcome to the home page!</p>
+    <Main></Main>
+  </SkyJs>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
