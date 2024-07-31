4587d13a7f658887589858f38fdf526372cf8cb7
aa <Nahida-aa@outlook.com>
2024-07-31 18:30:22 +0800

    debug

---
+++ timeline/2023/10/github_pages_nextjs/_src/pages/About/About.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('About')
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome to the about page!</p>
+</template>
+
+<style scoped>
+
+</style>
