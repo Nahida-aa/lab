07966dfe0816777ed3efca7719e4f0fddde1b39a
aa <Nahida-aa@outlook.com>
2024-08-02 18:21:01 +0800

    nav init and to-top

--- timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Home/Home.vue
+++ timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Home/Home.vue
@@ -8,13 +8,15 @@ const title = ref('Home')
 </script>
 
 <template>
-  <sky_js>
-    <h1>{{ title }}</h1>
-    <p>Welcome to the home page!</p>
-    <Main></Main>
-  </sky_js>
+  <sky_js></sky_js>
+  <h1>{{ title }}</h1>
+  <p>Welcome to the home page!</p>
+  <Main></Main>
 </template>
 
 <style scoped lang="scss">
-
+// .content {
+//   position: relative;
+//   z-index: 1; /* 确保内容在背景前面 */
+// }
 </style>
