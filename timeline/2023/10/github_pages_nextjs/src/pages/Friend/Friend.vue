acf6d9612eb0aaf7d7c86d8842d66931510d5ff9
aa <Nahida-aa@outlook.com>
2024-08-09 10:56:35 +0800

    nav

--- timeline/2023/10/github_pages_nextjs/src/pages/Friend/Friend.vue
+++ timeline/2023/10/github_pages_nextjs/src/pages/Friend/Friend.vue
@@ -1,3 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+import Main from '../../components/main/main.vue';
 </script>
+
+<template>
+  <Main>
+    <h1>Friend</h1>
+    <p>Welcome to the friend page!</p>
+  </Main>
+</template>
