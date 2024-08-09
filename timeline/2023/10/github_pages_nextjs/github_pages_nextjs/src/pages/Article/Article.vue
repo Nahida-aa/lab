cb3a7842c2cf2fa64720862f9eb4d5ed67069df0
aa <Nahida-aa@outlook.com>
2024-08-09 10:56:35 +0800

    nav

--- timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Article/Article.vue
+++ timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Article/Article.vue
@@ -1,3 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+import Main from '../../components/main/main.vue';
 </script>
+
+<template>
+  <Main>
+    <h1>Article</h1>
+    <p>Welcome to the article page!</p>
+  </Main>
+</template>
--- timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Guestbook/Guestbook.vue
+++ timeline/2023/10/github_pages_nextjs/github_pages_nextjs/src/pages/Guestbook/Guestbook.vue
@@ -1,3 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+import Main from '../../components/main/main.vue';
 </script>
+
+<template>
+  <Main>
+    <h1>Guestbook</h1>
+    <p>Welcome to the guestbook page!</p>
+  </Main>
+</template>
