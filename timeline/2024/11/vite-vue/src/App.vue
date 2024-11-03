5a7ed1a9583c408d92430b11e2609ac9637d82d7
aa <Nahida-aa@outlook.com>
2024-11-03 21:19:41 +0800

    ok

--- timeline/2024/11/vite-vue/src/App.vue
+++ timeline/2024/11/vite-vue/src/App.vue
@@ -1,30 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core"
+import Globe from "./components/Globe.vue";
+import {  OrbitControls } from "@tresjs/cientos";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <TresCanvas window-size antialias clear-color="#040d21">
+    <TresPerspectiveCamera :position="[ 0, 0, 400 ]" >
+      <TresAmbientLight :color="0xbbbbbb" :intensity="1" />
+      <TresDirectionalLight :color="0xffffff" :intensity="8" :position="[ -800, 2000, 400 ]" />
+      <TresDirectionalLight :color="0xffffff" :intensity="10" :position="[ -200, 500, 200 ]" />
+      <TresPointLight :color="0x7982f6" :intensity="50" :position="[ -200, 500, 200 ]" />
+    </TresPerspectiveCamera>
+    <Globe />
+    <OrbitControls
+      :enableDamping="true"
+      :dampingFactor="0.01"
+      :enablePan="false"
+      :min-distance="200"
+      :max-distance="500"
+      :rotate-speed="0.8"
+      :zoom-speed="1"
+      :auto-rotate="true" 
+      :min-polar-angle="Math.PI / 3.5"
+      :max-polar-angle="Math.PI - Math.PI / 3"
+    />
+  </TresCanvas>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  height: 100%;
+  width: 100%;
 }
 </style>
