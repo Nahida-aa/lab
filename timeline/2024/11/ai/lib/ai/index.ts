0270172df6b775b5ad577fe71c4adc841f9a772b
aa <Nahida-aa@outlook.com>
2024-11-21 18:20:04 +0800

    add: o1

--- timeline/2024/11/ai/lib/ai/index.ts
+++ timeline/2024/11/ai/lib/ai/index.ts
@@ -1,10 +1,11 @@
 import { createOpenAI } from '@ai-sdk/openai'
-const token = process.env.GITHUB_TOKEN;
-const endpoint = "https://models.inference.ai.azure.com";
+const openai_default_baseURL = "https://api.openai.com/v1"
+const openai_github_baseURL = "https://models.inference.ai.azure.com"
+const github_token = process.env.GITHUB_TOKEN
 const openai = createOpenAI({
   // custom settings, e.g.
   // compatibility: 'strict', // strict mode, enable when using the OpenAI API
-  baseURL: endpoint, apiKey: token
+  baseURL: openai_github_baseURL, apiKey: github_token
 });
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
--- timeline/2024/11/ai/lib/ai/models.ts
+++ timeline/2024/11/ai/lib/ai/models.ts
@@ -4,7 +4,7 @@ export interface Model {
   id: string;
   label: string;
   apiIdentifier: string;
-  description: string;
+  description?: string;
 }
 
 export const models: Array<Model> = [
@@ -12,14 +12,24 @@ export const models: Array<Model> = [
     id: 'gpt-4o-mini',
     label: 'GPT 4o mini',
     apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
+    // description: 'Small model for fast, lightweight tasks',
   },
   {
     id: 'gpt-4o',
     label: 'GPT 4o',
     apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
+    // description: "OpenAI's most advanced multimodal model in the GPT-4 famil Can handle both text and image inputs.",
   },
+  {
+    id: 'o1-mini',
+    label: 'o1 mini',
+    apiIdentifier: 'o1-mini',
+  },
+  {
+    id: 'o1-preview',
+    label: 'o1 preview',
+    apiIdentifier: 'o1-preview',
+  }
 ] as const;
 
 export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
