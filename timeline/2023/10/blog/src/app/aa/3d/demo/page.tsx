4e02cd8b04ad1f961a4125e2db810c664ee4b847
aa <Nahida-aa@outlook.com>
2025-02-11 14:12:11 +0800

    friend

---
+++ timeline/2023/10/blog/src/app/aa/3d/demo/page.tsx
@@ -0,0 +1,14 @@
+// import GlobeCanvas from '@/app/demo/globe/GlobeCanvas'
+import React from 'react'
+// import dynamic from 'next/dynamic'
+// const GlobeCanvas = dynamic(() => import('@/app/demo/globe/GlobeCanvas'), { ssr: false })
+
+
+export default function Home() {
+  return (
+    <main>
+      {/* <GlobeCanvas /> */}
+      3d
+    </main>
+  )
+}
