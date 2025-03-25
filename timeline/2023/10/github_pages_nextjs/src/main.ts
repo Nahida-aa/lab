abb6097086433726f808a68a90c76a85d8cc974c
xia-aa <xia-aa@outlook.com>
2025-03-25 17:20:20 +0800

    next init

--- timeline/2023/10/github_pages_nextjs/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.ts'
-// import './style.css'  // 确保导入样式文件
-
-createApp(App).use(router).mount('#app')
