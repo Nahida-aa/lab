fc9aad0509b2ec96662db9bedb129173fedf82da
aa <Nahida-aa@outlook.com>
2025-03-25 15:19:49 +0000

    gr

--- timeline/2023/10/github_pages_nextjs/app/[locale]/md/page.tsx
+++ timeline/2023/10/github_pages_nextjs/app/[locale]/md/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from 'react';
 import { LoadingS } from '@/components/ui/loading/Loading';
-// import { CustomMDX } from './mdx';
-// import { SerializeOptions } from 'node_modules/next-mdx-remote/dist/types';
-// import { getFile } from './lib/get';
-// import { notFound } from 'next/navigation';
+import { CustomMDX } from './mdx';
+import { SerializeOptions } from 'node_modules/next-mdx-remote/dist/types';
+import { getFile } from './lib/get';
+import { notFound } from 'next/navigation';
 import { title } from '@/components/primitives';
 import { MdxComp } from './types';
 // import 'katex/dist/katex.min.css';
@@ -18,11 +18,11 @@ export default async function Page ({
   // const { slug } = await params
   // const { page = '1', sort = 'asc', query = '' } = await searchParams
   const file_path = `app/[locale]/test.mdx`
-  // const { metadata, content, rawContent } = getFile(file_path)
+  const { metadata, content, rawContent } = getFile(file_path)
   // console.log("metadata:", metadata, "content:", content, "rawContent:", rawContent)
-  // if (!content) return notFound()
-  // const {content: JSXContent, frontmatter} = await CustomMDX({ source: rawContent })
-  const { default: Content, frontmatter: metadata } = await import("@/app/[locale]/md/test.mdx") as MdxComp
+  if (!content) return notFound()
+  const {content: JSXContent, frontmatter} = await CustomMDX({ source: rawContent })
+  // const { default: Content, frontmatter: metadata } = await import("@/app/[locale]/md/test.mdx") as MdxComp
     
   // const format = 'mdx';
   // const mdxOptions: SerializeOptions = {
@@ -34,7 +34,7 @@ export default async function Page ({
     <section className='mx-6'>
     <article className="prose dark:prose-invert  mx-auto max-w-full  ">
       <h1 className={`${title()} flex justify-center `}>{metadata.title}</h1>
-      <Content />
+      {JSXContent}
     </article>
     </section>
   </Suspense>
