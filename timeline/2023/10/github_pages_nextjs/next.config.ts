0a134538c4c73ad66281fa0805c88540cf88b36f
Nahida-aa <Nahida-aa@outlook.com>
2025-03-25 18:02:25 +0800

    link

---
+++ timeline/2023/10/github_pages_nextjs/next.config.ts
@@ -0,0 +1,55 @@
+import type { NextConfig } from 'next';
+import createMDX, { NextMDXOptions } from '@next/mdx'
+
+const rehypePrettyCode_options = {
+  // keepBackground: false, // 是否继承背景色
+  defaultLang: "plaintext",
+  // theme: moonlightTheme,
+  theme: {
+    dark: "github-dark-dimmed",
+    light: "github-light",
+  },
+  tokensMap: {
+    var: "variable",
+    str: "string",
+    fn: "entity.name.function",
+    cls: "entity.name.class",
+  },
+};
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      // [remarkGfm, {}],
+      ['remark-gfm'],
+      ['@vcarl/remark-headings'],
+      ['remark-frontmatter', {type: 'yaml', marker: '-'}], // 解析 frontmatter 到 语法树
+      ['remark-mdx-frontmatter'], // 导出 frontmatter
+      // ['remark-mermaidjs'],
+      // [remarkMath,{}],
+      ['remark-math', {}]
+    ],
+    rehypePlugins: [
+      ['rehype-callouts'],
+      // ['rehype-katex', { strict: true, throwOnError: true }]
+      // [rehypeMathjax,{}],
+      // ['rehype-mermaid'],
+      ['rehype-pretty-code', rehypePrettyCode_options],
+      ["rehype-mathjax"],
+    ],
+  },
+} as NextMDXOptions)
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+
+};
+
+const plugins = [
+  withMDX, // pnpm add @next/mdx, 仅需安装这个来实现
+  // withBundleAnalyzer, 
+]
+
+// module.exports = nextConfig; js 写法
+export default plugins.reduce((prev, item) => item(prev), nextConfig)
