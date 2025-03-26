d1345aa62c09739a078efe07e5148ad7b9ed4908
Nahida-aa <Nahida-aa@outlook.com>
2025-03-26 18:11:59 +0800

    test:r-mdx

--- timeline/2023/10/github_pages_nextjs/app/md/page.tsx
+++ timeline/2023/10/github_pages_nextjs/app/md/page.tsx
@@ -15,6 +15,7 @@ export default async function Page ({
 }) {
   // const { page = '1', sort = 'asc', query = '' } = await searchParams
   const file_path = `app/md/mdx-page/test.mdx`
+  const {default: MdxDoc} = await import(`@/public/blog/alg.json`)
   const { metadata, content, rawContent } = getFile(file_path)
   console.log("metadata:", metadata, "content:", content, "rawContent:", rawContent)
   if (!content) return notFound()
@@ -35,7 +36,7 @@ export default async function Page ({
   <section className='mx-6'>
   <article className="prose dark:prose-invert  mx-auto max-w-full  ">
     <h1 className={`${title()} flex justify-center `}>{metadata.title}</h1>
-    <CustomMDX source={rawContent} />
+    <CustomMDX source={MdxDoc.content} />
     {/* {JSXContent} */}
     {/* <Content /> */}
   </article>
