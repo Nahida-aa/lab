252bd1d8b0b9e72e08c59b774fac00d6e761d716
Nahida-aa <Nahida-aa@outlook.com>
2025-03-29 09:01:35 +0800

    fix:ios:pwa

--- timeline/2023/10/github_pages_nextjs/github_pages_nextjs/app/manifest.ts
+++ timeline/2023/10/github_pages_nextjs/github_pages_nextjs/app/manifest.ts
@@ -13,42 +13,35 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        "src": "/favicons/Nahida-48.webp",
+        "src": "/favicons/Nahida-48-r.webp",
         "sizes": "48x48", // 浏览器或操作系统的较小图标
         "type": "image/webp"
-      },
-      {
-        "src": "/favicons/Nahida-72.webp",
+      },{
+        "src": "/favicons/Nahida-72-r.webp",
         "sizes": "72x72", // Android 低分辨率设备的图标
         "type": "image/webp"
-      },
-      {
-        "src": "/favicons/Nahida-96.webp",
+      },{
+        "src": "/favicons/Nahida-96-r.webp",
         "sizes": "96x96", // Android 中等分辨率设备的图标
         "type": "image/webp"
-      },
-      {
-        "src": "/favicons/Nahida-128.webp",
+      },{
+        "src": "/favicons/Nahida-128-r.webp",
         "sizes": "128x128", // 桌面应用程序的图标
         "type": "image/webp"
-      },
-      {
-        "src": "/favicons/Nahida-144.webp",
+      },{
+        "src": "/favicons/Nahida-144-r.webp",
         "sizes": "144x144", // Windows 平台的 Pinned Tiles 图标
         "type": "image/webp"
-      },      
-      {
-        src: '/favicons/Nahida-192.webp',
+      },{
+        src: '/favicons/Nahida-192-r.webp',
         sizes: '192x192', // PWA 的主屏幕图标（必须提供）
         type: 'image/webp',
-      },
-      {
-        "src": "/favicons/Nahida-256.webp",
+      },{
+        "src": "/favicons/Nahida-256-r.webp",
         "sizes": "256x256", // 高分辨率桌面图标
         "type": "image/webp"
-      },
-      {
-        src: '/favicons/Nahida-512.webp',
+      },{
+        src: '/favicons/Nahida-512-r.webp',
         sizes: '512x512', // PWA 的高分辨率图标（必须提供）
         type: 'image/webp',
       },
