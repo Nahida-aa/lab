6eb83be3acbfa40a809f3823ac6371ab6950c58a
Nahida-aa <Nahida-aa@outlook.com>
2025-04-03 19:55:50 +0800

    up

--- timeline/2023/10/github_pages_nextjs/components/layout/sidebar/side-switcher.tsx
+++ timeline/2023/10/github_pages_nextjs/components/layout/sidebar/side-switcher.tsx
@@ -36,8 +36,8 @@ export function SideSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <GalleryVerticalEnd className="size-4" />
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg  bg-primary text-sidebar-primary-foreground">
+                <GalleryVerticalEnd className="size-4 " />
               </div>
               <div className="flex flex-col gap-0.5 leading-none">
                 <span className="font-semibold">一段待定文字</span>
