53c2a2b22afdac133881db608d7a7473fa83661d
Nahida-aa <Nahida-aa@outlook.com>
2025-04-11 21:12:42 +0800

    up

--- timeline/2023/10/github_pages_nextjs/docs/2023/06/13/1.py
+++ timeline/2023/10/github_pages_nextjs/docs/2023/06/13/1.py
@@ -2,11 +2,20 @@ from datetime import date, timedelta
 # print((date(2000, 5, 4) - date(2000, 1, 1)).days+1)
 
 # from datetime import date, timedelta
-t0 = date(1901, 1, 1)
-t1 = date(2000, 12, 31)
+# t0 = date(1901, 1, 1)
+# t1 = date(2000, 12, 31)
+# res = 0
+# while t0 <= t1:
+#     if t0.weekday() == 0:  # 0=Monday, 1=Tuesday, ..., 6=Sunday
+#         res += 1
+#     t0 += timedelta(days=1)
+# print(res)
+t0 = date(1900, 1, 1)
+t1 = date(9999, 12, 31)
+delta = timedelta(days=1)
 res = 0
-while t0 <= t1:
-    if t0.weekday() == 0:  # 0=Monday, 1=Tuesday, ..., 6=Sunday
+while t0 < t1:
+    if '2' in str(t0):
         res += 1
-    t0 += timedelta(days=1)
-print(res)
+    t0 += delta
+print(res+1)  # 输出: 1994240
